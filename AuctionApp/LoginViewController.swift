a972d0bdb4b164b17fb1dbbea4175f8e92b45333
Alexandra Barry <abarry@hubspot.com>
2016-12-06 02:51:57 -0500

    Require first and last name

--- AuctionApp/LoginViewController.swift
+++ AuctionApp/LoginViewController.swift
@@ -2,39 +2,49 @@ import UIKit
 
 class LoginViewController: UIViewController {
   
-  // let ref = FIRDatabase.database().reference()
   let loginToList = "LoginToList"
   
   @IBOutlet weak var emailTextField: UITextField!
   @IBOutlet weak var passwordTextField: UITextField!
   
   func signedIn(_ user: FIRUser?) {
-    // tell app we're logged in
     performSegue(withIdentifier: loginToList, sender: nil)
   }
-   
+  
   func setDisplayName(_ user: FIRUser) {
-   let changeRequest = user.profileChangeRequest()
-   changeRequest.displayName = user.email!.components(separatedBy: "@")[0]
-   changeRequest.commitChanges(){ (error) in
-    if let error = error {
-      print(error.localizedDescription)
-      return
+    
+    let alertController = UIAlertController(title: "What should we call you?", message: "", preferredStyle: .alert)
+    
+    alertController.addTextField { (textField : UITextField!) -> Void in
+      textField.placeholder = "First name"
     }
-    self.signedIn(FIRAuth.auth()?.currentUser)
-   }
+    alertController.addTextField { (textField : UITextField!) -> Void in
+      textField.placeholder = "Second name"
+    }
+    
+    let saveAction = UIAlertAction(title: "Save", style: .default, handler: { alert -> Void in
+      let firstTextField = alertController.textFields![0] as UITextField
+      let secondTextField = alertController.textFields![1] as UITextField
+      if (firstTextField.text!.characters.count > 0 && secondTextField.text!.characters.count > 0 ) {
+        let userData = ["email": user.email!, "firstname": firstTextField.text!, "lastname": secondTextField.text!] as [String : String]
+        FIRDatabase.database().reference().child("users").child(user.uid).setValue(userData)
+        self.signedIn(user)
+      }
+    })
+    
+    alertController.addAction(saveAction)
+    self.present(alertController, animated: true, completion: nil)
   }
   
   @IBAction func loginDidTouch(_ sender: AnyObject) {
     guard let email = emailTextField.text, let password = passwordTextField.text else { return }
     FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
-     if let error = error {
-      print(error.localizedDescription)
-      return
-     }
-     self.signedIn(user!)
+      if let error = error {
+        print(error.localizedDescription)
+        return
+      }
+      self.signedIn(user!)
     }
-    print("click")
   }
   
   @IBAction func signUpDidTouch(_ sender: AnyObject) {
@@ -44,17 +54,14 @@ class LoginViewController: UIViewController {
         print(error.localizedDescription)
         return
       }
-      let userData = ["email": email, "name": "A HubSpotter"] as [String : String]
-      // self.ref.child("users").child(email).setValue(userData)
       self.setDisplayName(user!)
     }
-    print("click")
   }
   
   override func viewDidAppear(_ animated: Bool) {
-   if let user = FIRAuth.auth()?.currentUser {
-    self.signedIn(user)
-   }
+    if let user = FIRAuth.auth()?.currentUser {
+      self.signedIn(user)
+    }
   }
 }
 
